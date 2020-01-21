9b98fbcfbe7f32790290af5f86de666cecca7fa1
paulemfag <boiteprofagot@gmail.com>
2020-01-21 15:55:16 +0100

    modifs

--- source.sql
+++ source.sql
@@ -1,12 +1,12 @@
 USE webDevelopment;
 ALTER TABLE languagess ADD versions VARCHAR(10);
-ALTER TABLE frameworks ADD version INT;
+ALTER TABLE frameworks ADD IF NOT EXISTS version INT;
 ALTER TABLE languagess RENAME COLUMN "versions" TO "version";
 ALTER TABLE frameworks RENAME COLUMN "name" TO "framework";
 ALTER TABLE frameworks MODIFY COLUMN version VARCHAR(10);
 USE codex;
 ALTER TABLE clients DROP COLUMN "secondPhoneNumber";
 ALTER TABLE clients RENAME COLUMN "firstPhoneNumber" TO "phoneNumber";
-ALTER TABLE frameworks MODIFY COLUMN phoneNumber VARCHAR(100);
+ALTER TABLE clients MODIFY COLUMN phoneNumber VARCHAR(15);
 ALTER TABLE clients ADD zipcode VARCHAR(5);
 ALTER TABLE clients ADD city VARCHAR(25);
